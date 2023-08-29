c18606108e524d2b6f78d189bc2e8c5243235dc6
XORandom <40043238+XORandom@users.noreply.github.com>
2023-08-29 14:35:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 Сумма первой половины цифр равна сумме второй половине цифр.
 Работает только с четным количеством цифр в билете.
 """
-
+import tqdm
 lucky_numbers_list = []
-length_of_number = 4
+length_of_number = 6
 
 
 def sum_of_digits(n):
@@ -18,9 +18,7 @@ def sum_of_digits(n):
 def find_lucky_numbers():
     cnt = 0
     for i in range(1, 10**length_of_number):
-        left = i // (10**(length_of_number/2))  # Левая половина
-        right = i % (10**(length_of_number/2))  # Правая половина
-        if sum_of_digits(left) == sum_of_digits(right):
+        if sum_of_digits(i // (10**(length_of_number/2))) == sum_of_digits(i % (10**(length_of_number/2))):
             cnt += 1
             lucky_numbers_list.append(i)
     return cnt
